126cf4a421a1febc4b3f37ce2ad8e411c97bcbe7
farheenfarheen <90678000+farheenfarheen@users.noreply.github.com>
2021-09-17 10:29:14 +0530

    sorting

---
+++ ascending.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int n,temp=0,a[10];
+	//intailizing size of array
+	scanf("%d",&n);
+	//intaializing array
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(int i=0;i<n;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			if(a[i]<a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	//printing sorting elements
+	for(int i=0;i<n;i++)
+	{
+		printf("%d",a[i]);
+	}
+}
